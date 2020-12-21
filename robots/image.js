42e9e49fd049983ecee8c5c8a1864d0011e65d95
Davi Lucas <davilucasdlcreator@gmail.com>
2020-12-21 16:36:14 -0300

    Baixar as imagens adicionado

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
 const state = require('./state.js');
@@ -8,6 +9,7 @@ async function robot(){
     const content = state.load();
     await fetchImageOfAllSentences(content);
 
+    await downloadAllImages(content);
     state.save(content)
 
     async function fetchImageOfAllSentences(content){
@@ -31,6 +33,38 @@ async function robot(){
         const imagesUrl = response.data.items.map(item => item.link)
 
         return imagesUrl;
+    }
+
+    async function downloadAllImages(content){
+        content.downloadedImages = [];
+
+        for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+            const images = content.sentences[sentenceIndex].images;
+
+            for(let imageIndex = 0; imageIndex < images.length; imageIndex++){
+                const imageUrl = images[imageIndex];
+
+                try {
+                    if(content.downloadedImages.includes(imageUrl)){
+                        throw new Error("Image already downloaded")
+                    }
+
+                    await downloadAndSave(imageUrl, `${sentenceIndex}--original.png`);
+                    content.downloadedImages.push(imageUrl)
+                    console.log(`> [image-robot] [${sentenceIndex}][${imageIndex}] Image successfully downloaded: ${imageUrl}`)
+                    break;
+                }catch(error){
+                    console.log(`> [image-robot] [${sentenceIndex}][${imageIndex}] Error (${imageUrl}): ${error}`)
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, fileName){
+        return imageDownloader.image({
+            url, url,
+            dest:`./content/${fileName}`,
+        })
     }
 }
 
